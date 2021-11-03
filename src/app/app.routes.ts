f78aeb20bf29b97d20ecbe921f2496290a763d9f
Wesley Queiroz <wesleygomesqueiroz@gmail.com>
2021-11-03 17:03:02 -0300

    Pagina About

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from '@src/app/pages/about/about.component';
 
 
 export const routes: Routes = [
   {
-      path: '',
-      redirectTo: '/home',
-      pathMatch: 'full',
+    path: '',
+    redirectTo: '/about',
+    pathMatch: 'full',
   },
+  {
+    path: "about",
+    component: AboutComponent
+  }
 ];
