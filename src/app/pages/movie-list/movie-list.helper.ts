6e892a57bb335257b91a2b82ec279dddf67fc414
Wesley Queiroz <wesleygomesqueiroz@gmail.com>
2021-11-04 22:47:40 -0300

    Excluir fimes (Mobile)

---
+++ src/app/pages/movie-list/movie-list.helper.ts
@@ -0,0 +1,7 @@
+export class MovieListHelper {
+  static showDeleteMovieConfirmation(message: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      resolve(confirm(message))
+    });
+  }
+}
