d47fc6ba247c08e82d08862b8c674f91130f415a
Wesley Queiroz <wesleygomesqueiroz@gmail.com>
2021-11-06 21:45:59 -0300

    Criação do formulario de edição

--- src/app/app.module.tns.ts
+++ src/app/app.module.tns.ts
@@ -1,13 +1,15 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NativeScriptFormsModule, NativeScriptHttpClientModule, NativeScriptModule } from '@nativescript/angular';
 import { AppRoutingModule } from '@src/app/app-routing.module';
 import { AppComponent } from '@src/app/app.component';
 import { InMemoryDataService } from '@src/app/in-memory-data.service';
 import { AboutComponent } from '@src/app/pages/about/about.component';
+import { MovieFormComponent } from '@src/app/pages/movie-form/movie-form.component';
 import { MovieListComponent } from '@src/app/pages/movie-list/movie-list.component';
+import { MobActionBarComponent } from '@src/app/shared/mob-action-bar/mob-action-bar.component';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { NativeScriptUISideDrawerModule } from 'nativescript-ui-sidedrawer/angular';
-import { MobActionBarComponent } from '@src/app/shared/mob-action-bar/mob-action-bar.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,7 @@ import { MobActionBarComponent } from '@src/app/shared/mob-action-bar/mob-action
     AboutComponent,
     MovieListComponent,
     MobActionBarComponent,
+    MovieFormComponent,
   ],
   imports: [
     NativeScriptModule,
@@ -22,6 +25,7 @@ import { MobActionBarComponent } from '@src/app/shared/mob-action-bar/mob-action
     NativeScriptHttpClientModule,
     NativeScriptFormsModule,
     NativeScriptUISideDrawerModule,
+    ReactiveFormsModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { put204: false, dataEncapsulation: false })
   ],
   providers: [],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,18 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from '@src/app/pages/about/about.component';
+import { MovieFormComponent } from '@src/app/pages/movie-form/movie-form.component';
 import { MovieListComponent } from '@src/app/pages/movie-list/movie-list.component';
 
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/movies',
+    redirectTo: '/movies/2/edit',
     pathMatch: 'full',
+  },
+  {
+    path: "movies/:id/edit",
+    component: MovieFormComponent
   },
   {
     path: "about",
