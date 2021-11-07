2b9e69d223d2dc625590c33fd168f6cc8c92c70a
Wesley Queiroz <wesleygomesqueiroz@gmail.com>
2021-11-07 18:18:56 -0300

    lista inicial do app

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,12 +3,7 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     let movies = [
-      { id: 1, title: "Homen de Ferro", obs: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", rating: 5, toWatchNext: true },
-      { id: 2, title: "Motoqueiro Fantasma", obs: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", rating: 3, toWatchNext: false },
-      { id: 3, title: "Matrix", obs: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", rating: 3.5, toWatchNext: true },
-      { id: 4, title: "M.I.B.", obs: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", rating: null, toWatchNext: false },
-      { id: 5, title: "Homen Aranha", obs: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", rating: 4, toWatchNext: false },
-      { id: 6, title: "Venon", obs: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", rating: 5, toWatchNext: true },
+      { id: 1, title: "Título do filme", obs: "Descrição do filme", rating: 3, toWatchNext: true },
     ];
     return { movies };
   }
